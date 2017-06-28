54da1d4f6226678114d58cbc26726e82d04037d0
Tonia Roddick <troddick@USGS.gov>
2017-06-28 10:27:32 -0500

    added scrollOffset

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
 import { Component, ViewChild } from '@angular/core';
-import {NavbarComponent} from './navbar/navbar.component';
-import {MainviewComponent} from './mainview/mainview.component';
-import {SidebarComponent} from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MainviewComponent } from './mainview/mainview.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { PageScrollConfig } from 'ng2-page-scroll';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-   @ViewChild(NavbarComponent) navbarComponent: NavbarComponent;
-   @ViewChild(SidebarComponent) sidebarComponent: SidebarComponent;
-   @ViewChild(MainviewComponent) mainviewCommponent: MainviewComponent;
+	constructor() {
+		PageScrollConfig.defaultScrollOffset = 85;
+	}
+	@ViewChild(NavbarComponent) navbarComponent: NavbarComponent;
+	@ViewChild(SidebarComponent) sidebarComponent: SidebarComponent;
+	@ViewChild(MainviewComponent) mainviewCommponent: MainviewComponent;
 }

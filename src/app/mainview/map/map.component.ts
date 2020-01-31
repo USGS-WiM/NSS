a0b90fff2897f96b521afa7b7dd8821bcdc12029
amedenblik <amedenblik@usgs.gov>
2020-01-31 16:14:33 -0500

    add initial basemap control

--- src/app/mainview/map/map.component.ts
+++ src/app/mainview/map/map.component.ts
@@ -13,17 +13,33 @@ export class MapComponent implements OnInit {
 
   ngOnInit() {
 
-    this.map = L.map('map', {
-      center: [39.8282, -98.5795],
-      zoom: 3
+    const mbAttr = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+      '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      // tslint:disable-next-line:max-line-length
+      mbUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
+
+    const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    });
+    const grayscale = L.tileLayer(mbUrl, { id: 'mapbox.light', attribution: mbAttr });
+    const streets = L.tileLayer(mbUrl, { id: 'mapbox.streets', attribution: mbAttr });
+
+    this.map = new L.Map('map', {
+      center: new L.LatLng(39.8283, -98.5795),
+      zoom: 4,
+      layers: [streets]
     });
 
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
+    const baseMaps = {
+      'Open Street Map': osm,
+      'Grayscale': grayscale,
+      'Streets': streets
+    };
 
-    tiles.addTo(this.map);
+    var layerControl = L.control.layers(baseMaps);
+    this.map.addControl(layerControl);
+   // L.control.layers(baseMaps, null, { position: 'topright' }).addTo(this.map);
   }
 
 }

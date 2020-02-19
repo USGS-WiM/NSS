65062a2cf10a42e1b6c42d4a35c3ca700961fcf9
amedenblik <amedenblik@usgs.gov>
2020-02-19 11:07:46 -0500

    create addregresionregion component

---
+++ src/app/shared/components/addregressionregion/addregressionregion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addregressionregion',
+  templateUrl: './addregressionregion.component.html',
+  styleUrls: ['./addregressionregion.component.css']
+})
+export class AddregressionregionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

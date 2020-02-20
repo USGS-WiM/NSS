6b4089a9a7018c69ea134698e196fd4fff87c80b
amedenblik <amedenblik@usgs.gov>
2020-02-20 12:19:33 -0500

    move remaining map component to addregreg modal

--- src/app/shared/components/addregressionregion/addregressionregion.component.ts
+++ src/app/shared/components/addregressionregion/addregressionregion.component.ts
@@ -16,6 +16,9 @@ import { ConfigService } from 'app/config.service';
 import { ToasterService } from 'angular2-toaster';
 import { AuthService } from 'app/shared/services/auth.service';
 import { Region } from '../../interfaces/region';
+import * as L from 'leaflet';
+import * as shp from 'shpjs';
+import { GeojsonService } from '../../services/geojson.service';
 
 @Component({
   selector: 'addRegressionRegionModal',
@@ -52,10 +55,18 @@ export class AddRegressionRegionModal implements OnInit, OnDestroy {
   public newRegRegForm: FormGroup;
   public newCitForm: FormGroup;
   public regions;
+  public map;
+  private input;
+  private file;
 
-  constructor(private _nssService: NSSService, private _modalService: NgbModal, private _fb: FormBuilder,
-    private _settingsService: SettingsService, private _configService: ConfigService, private _toasterService: ToasterService,
-    private _authService: AuthService) {
+  constructor(private _nssService: NSSService,
+              private _modalService: NgbModal,
+              private _fb: FormBuilder,
+              private _settingsService: SettingsService,
+              private _configService: ConfigService,
+              private _toasterService: ToasterService,
+              private _authService: AuthService,
+              private geojsonService: GeojsonService) {
     this.configSettings = this._configService.getConfiguration();
     this.newRegRegForm = _fb.group({
       name: new FormControl(null, Validators.required),
@@ -96,6 +107,42 @@ export class AddRegressionRegionModal implements OnInit, OnDestroy {
 
     this.modalElement = this.addRegressionRegionModal;
 
+  }
+
+  public loadMap() {
+    this.uploadPolygon = true;
+
+    // Initialize basemap layers
+    const tileLayer_topography = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+      attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+    });
+    const tileLayer_streets = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+      attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
+    });
+    const tileLayer_grayscale = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+      attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ'
+    });
+    const tileLayer_satellite = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    });
+
+    // Initialize map 
+    this.map = new L.Map('map', {
+      center: new L.LatLng(39.8283, -98.5795),
+      zoom: 4,
+      layers: [tileLayer_streets]
+    });
+
+    // Create basemap collection
+    const baseMaps = {
+      'Streets': tileLayer_streets,
+      'Topography': tileLayer_topography,
+      'Grayscale': tileLayer_grayscale,
+      'Satellite': tileLayer_satellite
+    };
+
+    // Add basemap control to the map
+    L.control.layers(baseMaps).addTo(this.map);
   }
 
   public getRegRegions() {
@@ -235,6 +282,44 @@ export class AddRegressionRegionModal implements OnInit, OnDestroy {
   public uploadNewPolygon(rr) {
     // Upload new polygon
     // TODO: Send polygon to services
+  }
+
+  public addGeojsonToMap(polygon: any) {
+    const polygonLayer = L.geoJSON(polygon, {
+      style: (feature) => ({
+        weight: 3,
+        opacity: 0.5,
+        color: '#000000',
+        fillOpacity: 0.8,
+        fillColor: '#fc8d62'
+      })
+    });
+
+    this.map.addLayer(polygonLayer);
+    this.map.fitBounds(polygonLayer.getBounds());
+  }
+
+  async SHPtoGEOJSON(form: any) {
+    const self = this;
+    await this.geojsonService.readFileContent(this.file)
+      .toPromise().then(
+        res => {
+          shp(res).then(function (geojson) {
+            self.addGeojsonToMap(geojson);
+          })
+        }
+      );
+  }
+
+  public loadFile() {
+    this.input = document.getElementById('fileinput');
+    if (!this.input.files[0]) {
+      alert("Please select a file.");
+    }
+    else {
+      this.file = this.input.files[0];
+      this.SHPtoGEOJSON(this.file);
+    }
   }
 
 

f3966940de5c7b007919ee81c33f125a0fc61bae
Wavra <hwavra@usgs.gov>
2020-10-07 11:39:15 -0500

    added to settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -54,6 +54,7 @@ import { GagestatsComponent } from './gagestats/gagestats.component';
 import { GsSidebarComponent } from './gagestats/gs-sidebar/gs-sidebar.component';
 import { GagestatsService } from './gagestats/gagestats.service';
 import { GagepageComponent } from './gagestats/gagepage/gagepage.component';
+import { MethodsComponent } from './settings/categories/methods/methods.component';
 
 
 declare const require: any;
@@ -73,7 +74,8 @@ const appRoutes: Routes = [
       { path: 'managers', component: ManagersComponent },
       { path: 'errors', component: ErrorsComponent},
       { path: 'agencies', component: AgenciesComponent},
-      { path: 'stationtypes', component: StationTypesComponent}
+      { path: 'stationtypes', component: StationTypesComponent},
+      { path: 'methods', component: MethodsComponent}
     ],
     runGuardsAndResolvers: 'always'
   },
@@ -100,7 +102,7 @@ export function highchartsFactory() {
     AppComponent, MainviewComponent, SidebarComponent, SettingsComponent, AboutModal, UniquePipe, StatisticGroupsComponent,
     MathjaxDirective, RegressionTypesComponent, UnitTypesComponent, UnitSystemsComponent, VariableTypesComponent, ManagersComponent,
     ProfileComponent, ErrorsComponent, RegionsComponent, AddScenarioModal, ManageCitationsModal, CitationFilterPipe, AddRegressionRegionModal, 
-    LoaderComponent, GagestatsComponent, GsSidebarComponent, AgenciesComponent, StationTypesComponent, GagepageComponent
+    LoaderComponent, GagestatsComponent, GsSidebarComponent, AgenciesComponent, StationTypesComponent, GagepageComponent, MethodsComponent
   ],
   imports: [
     NgSelectModule, BrowserModule, FormsModule, HttpClientModule, ToasterModule, BrowserAnimationsModule, ReactiveFormsModule, MultiselectDropdownModule,

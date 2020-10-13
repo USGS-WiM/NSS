299161bf40bbe5541931bc5a7c8242e290f1a831
09Kathy <kathymdooley@gmail.com>
2020-10-13 12:51:43 -0500

    Issue #182 - removes extra code

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -257,12 +257,6 @@ export class SidebarComponent implements OnInit {
 
     // submit / Compute button click
     public CalculateScenario(): void {
-        /*
-        if (this.originalRegRegion[0] != null) {
-            this.scenarios[0].regressionRegions = this.originalRegRegion;
-        }
-        */
-        console.log("this.scenarios", this.scenarios);
         this.originalRegRegion = this.scenarios[0].regressionRegions;
         this._nssService.showTempRegRegion(this.originalRegRegion);
         this._loaderService.showFullPageLoad();

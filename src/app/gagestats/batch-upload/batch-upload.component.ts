2bc9cc04f4d14f512352884427ef25ca347c2b0c
Anders <ahopkins@contractor.usgs.gov>
2020-11-05 15:34:38 -0600

    End of day progress 11/5
    Station section is pretty near complete. Http post request function is in place, but the request it failing for some reason; probably to do with the isRegulated category.  Data in the table can be edited and the dropdown menus allow the user to select the data type for each of the data columns.

--- src/app/gagestats/batch-upload/batch-upload.component.ts
+++ src/app/gagestats/batch-upload/batch-upload.component.ts
@@ -6,8 +6,10 @@ import { SettingsService } from 'app/settings/settings.service';
 import { ConfigService } from 'app/config.service';
 import { ToasterService } from 'angular2-toaster';
 import { Config } from 'app/shared/interfaces/config';
-import * as XLSX from 'xlsx'
-import { skip } from 'rxjs/operators';
+import * as XLSX from 'xlsx';
+import { Agency } from 'app/shared/interfaces/agencies';
+import { StationType } from 'app/shared/interfaces/stationtypes';
+import { Region } from 'app/shared/interfaces/region';
 
 @Component({
   selector: 'batchUploadModal',
@@ -27,12 +29,15 @@ export class BatchUploadModal implements OnInit {
   public tableDisplay: boolean = false;
   public tableEdit: boolean = false;
   public data: [][];
-  public stationChars;
-  public statChars;
-  public charChars;
+  public stationChars = {"Agency": "agencyID", "Code": "code", "IsRegulated": "isRegulated", "Latitude": "latitude", "Longitude": "longitude", "Name": "name", "Region": "regionID", "Station Type": "stationTypeID"};
+  public statChars = ["statisticGroupTypeID", "regressionTypeID", "stationID", "value", "unitTypeID", "comments", "isPreferred", "yearsofRecord"];
+  public charChars = ["stationID", "variableTypeID", "unitTypeID", "value", "comments"];
   public headers;
-  public selectedChars;
+  public selectedChars = [];
   public tableData;
+  public agencies: Array<Agency>;
+  public regions: Array<Region>;
+  public stationTypes: Array<StationType>;
 
   constructor(private _nssService: NSSService, private _modalService: NgbModal, private _fb: FormBuilder,
     private _settingsService: SettingsService, private _configService: ConfigService, private _toasterService: ToasterService) {
@@ -45,10 +50,19 @@ export class BatchUploadModal implements OnInit {
       if (show) { this.showModal(); }
     });
     this.modalElement = this.batchUploadModal;
-    this.stationChars = ["Code", "Agency", "Name", "Station Type", "Latitude", "Longitude", "IsRegulated"];
-    this.statChars = ["statisticGroupTypeID", "regressionTypeID", "stationID", "value", "unitTypeID", "comments", "isPreferred", "yearsofRecord"];
-    this.charChars = ["stationID", "variableTypeID", "unitTypeID", "value", "comments"];
+    // subscribe to all agencies
+    this._nssService.agencies.subscribe((agencies: Array<Agency>) => {
+      this.agencies = agencies;
+    });
+    // subscribe to all station types
+    this._nssService.stationTypes.subscribe((stationtypes: Array<StationType>) => {
+      this.stationTypes = stationtypes;
+    });
+    this._settingsService.getEntitiesGageStats(this.configSettings.regionURL).subscribe((regions: Array<Region>) => {
+      this.regions = regions;
+    });
 }
+
 
   public showModal(): void {
     this.modalRef = this._modalService.open(this.modalElement, { backdrop: 'static', keyboard: false, size: 'lg', windowClass: 'modal-xl' });
@@ -75,48 +89,89 @@ export class BatchUploadModal implements OnInit {
   }
 
   public submitStations() {
-    //console.log(this.tableData);
     this.selectedChars;
     var stations;
     this.tableData.forEach(row => { // Loop through the rows of the table
-        if (row == this.tableData[0]) {
-            return;
-        } else {    
-            var i = 0;
-            var station;
-            row.forEach(x => {  // Loop thru the cells of each row
-                const item = '"' + this.tableData[0][i] + '": "' + x + '"'; // Assign the headers as the keys, the values as values
-                if (i == 0){
-                    station = item;
-                    i ++;
-                } else {
-                    station = station + ', ' + item;
-                    i ++;
-                }
-            })
-            var stationObj = JSON.parse('{' + station + '}');  // Parse strings into JSON objects
-            const location = {type: 'Point', coordinates: [ stationObj.Latitude, stationObj.Longitude]};  // Add location item
-            delete stationObj.Latitude;  // Delete old location items
-            delete stationObj.Longitude;
-            stationObj = {...stationObj, 'Location': location};
-            //console.log(stationObj);
-            if (stations == null) {  // Group station objects into an array
-                stations = [stationObj];
-            } else {
-                stations = [...stations, stationObj ];
-            }  
-        }  
+      if (row == this.tableData[0]) {
+          return;
+      } 
+      else {    
+          var i = 0;
+          var station;
+          row.forEach(x => {  // Loop thru the cells of each row
+              const item = '"' + this.tableData[0][i] + '": "' + x + '"'; // Assign the headers as the keys, the values as values
+              if (i == 0){
+                  station = item;
+                  i ++;
+              } else {
+                  station = station + ', ' + item;
+                  i ++;
+              }
+          })
+          var stationObj = JSON.parse('{' + station + '}');  // Parse strings into JSON objects
+          const location = {type: 'Point', coordinates: [ parseFloat(stationObj.longitude), parseFloat(stationObj.latitude) ]};  // Add location item
+          delete stationObj.latitude;  // Delete old location items
+          delete stationObj.longitude;
+          stationObj = {...stationObj, 'location': location};
+          if (stationObj.agencyID) {
+            var aID = this.getAgencyID(stationObj.agencyID);
+            stationObj.agencyID = aID;
+          }
+          if (stationObj.stationTypeID) {
+            var sID = this.getStationTypeID(stationObj.stationTypeID);
+            stationObj.stationTypeID = sID;
+          }
+          if (stationObj.regionID) {
+            var rID  = this.getRegionID(stationObj.regionID);
+            stationObj.regionID = rID;
+          }
+          if (stations == null) {  // Group station objects into an array
+              stations = [stationObj];
+          } 
+          else {
+              stations = [...stations, stationObj ];
+          }  
+      }  
     });    
     console.log(stations)
+    this._settingsService.postEntityGageStats(stations, "stations/Batch")
+      .subscribe((response:any) =>{
+        if(!response.headers){
+          this._toasterService.pop('info', 'Info', 'Items Added');
+        } else {
+          this._settingsService.outputWimMessages(response);
+        }});
+    
   }
 
   public createTable = function(data){
-    this.headers = data[0]
-    //delete data[0]
-    this.tableData = data 
-    console.log(this.tableData)
+    this.headers = JSON.parse(JSON.stringify(data[0]));  // copy the first row of the excel sheet as a list of headers
+    this.tableData = JSON.parse(JSON.stringify(data));   // copy the data from the excel sheet to display and change
+    // FIX THIS!!!!!!!!
+    this.tableData[0] = ['','','','','','','',''];
+    //console.log(this.tableData);
+  }
 
-    
+  public getKeys(obj) {
+    return Object.keys(obj);
+  }
+
+  public getStationTypeID(code) {
+    if (this.stationTypes) {
+      return (this.stationTypes.find(s => s.code === code).id);
+    }
+  }
+
+  public getAgencyID(code) {
+    if (this.agencies) {
+      return (this.agencies.find(a => a.code === code).id);
+    }
+  }
+
+  public getRegionID(name) {
+    if (this.regions) {
+      return (this.regions.find(r => r.name === name).id);
+    }
   }
 
 }
